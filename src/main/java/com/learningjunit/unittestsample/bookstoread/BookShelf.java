4f99dfe0c70cc5d9c96d94bb1f2e9ac5fc878e33
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-05-18 13:57:10 +0800

    Chapter 3.

--- src/main/java/com/learningjunit/unittestsample/bookstoread/BookShelf.java
+++ src/main/java/com/learningjunit/unittestsample/bookstoread/BookShelf.java
@@ -1,10 +1,41 @@
 package com.learningjunit.unittestsample.bookstoread;
 
-import java.util.Collections;
-import java.util.List;
+import java.time.Year;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class BookShelf {
-    public List<String> books() {
-        return Collections.emptyList();
+
+
+
+    private final List<Book> books = new ArrayList<>();
+
+    public List<Book> books() {
+      return Collections.unmodifiableList(books);
+
+    }
+
+    public void add(Book... bookToAdd) {
+        Arrays.stream(bookToAdd).forEach(books::add);
+    }
+
+    public List<Book> arrange() {
+        return arrange(Comparator.naturalOrder());
+    }
+    public List<Book> arrange(Comparator<Book> criteria) {
+        return books.stream().sorted(criteria).collect(Collectors.toList());
+    }
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return groupBy(book -> Year.of(book.getPublishedOn().getYear()));
+    }
+
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+        return books
+                .stream()
+                .collect(groupingBy(fx));
     }
 }
